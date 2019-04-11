aba87bb94d30af9e67280febaeeefde100fce6f8
danialaqil <danial_aqil@mymail.sutd.edu.sg>
2019-04-12 00:27:34 +0800

    sgsg

--- src/app/before-schedule-display/before-schedule-display.component.spec.ts
+++ src/app/before-schedule-display/before-schedule-display.component.spec.ts
@@ -1,6 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BeforeScheduleDisplayComponent } from './before-schedule-display.component';
+import {FormsModule} from '@angular/forms';
+import {MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+
+    MatCardModule,
+    MatButtonModule,
+    MatTableModule,
+    MatCommonModule,
+    MatInputModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatProgressSpinnerModule,
+    MatRadioModule} from '@angular/material';
 
 describe('BeforeScheduleDisplayComponent', () => {
   let component: BeforeScheduleDisplayComponent;
@@ -8,6 +23,20 @@ describe('BeforeScheduleDisplayComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule,MatSidenavModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatListModule,
+    
+        MatCardModule,
+        MatButtonModule,
+        MatTableModule,
+        MatCommonModule,
+        MatInputModule,
+        MatDialogModule,
+        MatMenuModule,
+        MatProgressSpinnerModule,
+        MatRadioModule],
       declarations: [ BeforeScheduleDisplayComponent ]
     })
     .compileComponents();
